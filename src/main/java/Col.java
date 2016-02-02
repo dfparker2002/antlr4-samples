26385b8f54960e7ce815e6fb1e26c6c8e1a30334
rfqu <alexei.kaigorodov@googlemail.com>
2016-02-02 18:32:56 +0300

    reads data from file

--- src/main/java/Col.java
+++ src/main/java/Col.java
@@ -6,17 +6,48 @@
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/tpantlr2 for more book information.
 ***/
+import java.io.InputStream;
+import java.io.PrintStream;
+
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+import antlr4.RowsBaseListener;
+import antlr4.RowsLexer;
+import antlr4.RowsParser;
+import antlr4.RowsParser.RowContext;
 
 public class Col {
     public static void main(String[] args) throws Exception {
-        ANTLRInputStream input = new ANTLRInputStream(System.in);
+        String resource = "/t.rows";
+        InputStream fileInput = Col.class.getResourceAsStream(resource);
+        if (fileInput==null) {
+            throw new IllegalStateException("resource file not found:"+resource); 
+        };
+        ANTLRInputStream input = new ANTLRInputStream(fileInput);
         RowsLexer lexer = new RowsLexer(input);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-        int col = Integer.valueOf(args[0]);
+        int col = 2;//Integer.valueOf(args[0]);
         RowsParser parser = new RowsParser(tokens, col); // pass column number!
-        parser.setBuildParseTree(false); // don't waste time bulding a tree
+        ParseListener parseListener = new ParseListener();
+        parser.setBuildParseTree(false); // don't waste time building a tree
+        parser.addParseListener(parseListener);
         parser.file(); // parse
+    }
+    
+    static class ParseListener extends RowsBaseListener {
+       static PrintStream out=System.out;
+
+        @Override
+        public void exitRow(RowContext ctx) {
+            out.println("exitRow: start="+ctx.start.getText()+"; end="+ctx.stop.getText());
+        }
+
+        @Override
+        public void visitTerminal(TerminalNode node) {
+            out.println("visitTerminal: node="+node);
+        }
+        
     }
 }
